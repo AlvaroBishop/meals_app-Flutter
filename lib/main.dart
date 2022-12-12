3fea02397567035108aa223c5c39bd299663a740
AlvaroBishop <bishopgamma@gmail.com>
2022-12-11 22:29:25 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/categories_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = ThemeData();
+    return MaterialApp(
+      title: 'Meals App',
+      theme: ThemeData(
+        fontFamily: 'Raleway'
+        ).copyWith(
+              textTheme: ThemeData.light().textTheme.copyWith(
+                bodySmall: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                bodyMedium: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                titleLarge: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'RobotoCondensed',
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black
+                ),
+              ),
+              canvasColor: Color.fromRGBO(255, 254, 229, 1),
+              colorScheme: theme.colorScheme.copyWith(
+                primary: Colors.orangeAccent,
+                secondary: Colors.amber,
+
+                ),
+              
+              ),
+      home: CategoriesScreen(),
+
+    );
+  }
+}
+
